fc4a4a7a9af58e66cb225fc278ea2c88e87802cb
Guanlun Zhao <guanlunzhao@gmail.com>
2018-02-25 11:48:40 -0600

    draw whole sprites

--- src/main.js
+++ src/main.js
@@ -26,98 +26,126 @@ import Config from './config';
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+// const sprites = {
+//     head: {
+//         path: '../images/sprites/PNG/all_body_parts/silver_helmet.png',
+//     },
+//     torso: {
+//         path: '../images/sprites/PNG/all_body_parts/silver_body.png',
+//     },
+//     foot: {
+//         path: '../images/sprites/PNG/all_body_parts/foot.png',
+//     },
+// };
+
+const spriteDir = '../images/sprites/whole';
+
 const sprites = {
-    head: {
-        path: '../images/sprites/PNG/all_body_parts/silver_helmet.png',
-    },
-    torso: {
-        path: '../images/sprites/PNG/all_body_parts/silver_body.png',
-    },
-    foot: {
-        path: '../images/sprites/PNG/all_body_parts/foot.png',
-    },
+    // attack: {
+    //     baseName: 'Attack',
+    //     sequence: [],
+    // },
+    idle: [],
+    attack: [],
+    walk: [],
 };
 
-let walkingCycle = 0;
+for (const spriteKey of Object.keys(sprites)) {
+    const fileBaseName = spriteKey.charAt(0).toUpperCase() + spriteKey.slice(1);
 
-function renderCharacter() {
+    for (let i = 0; i < 10; i++) {
+        sprites[spriteKey].push({
+            action: spriteKey,
+            frameIdx: i,
+            filePath: `${spriteDir}/${fileBaseName} (${i + 1}).png`,
+        });
+    }
+}
+
+// let isWalking = false;
+// let walkingCycle = 0;
+let animCycle = 0;
+
+// function renderCharacter() {
+//     ctx.save();
+//     ctx.scale(0.5, 0.5);
+
+//     ctx.translate(200, 200);
+//     ctx.drawImage(sprites.torso.img, 0, 0);
+
+//     ctx.save();
+//     ctx.translate(-20, -160);
+//     ctx.drawImage(sprites.head.img, 0, 0);
+//     ctx.restore();
+
+//     ctx.save();
+//     const rightFootOffset = Math.sin(walkingCycle / 25 * Math.PI) * 3;
+//     ctx.translate(160, 180 + rightFootOffset);
+//     ctx.drawImage(sprites.foot.img, 0, 0);
+//     ctx.restore();
+
+//     ctx.save();
+//     const leftFootOffset = Math.sin((25 + walkingCycle / 25) * Math.PI) * 3;
+//     ctx.translate(40, 180 + leftFootOffset);
+//     ctx.drawImage(sprites.foot.img, 0, 0);
+//     ctx.restore();
+
+//     ctx.restore();
+
+//     if (isWalking || walkingCycle > 0) {
+//         walkingCycle++;
+//         if (walkingCycle > 50) {
+//             walkingCycle = 0;
+//         }
+//     }
+// }
+
+function renderSprite() {
     ctx.save();
-    ctx.scale(0.5, 0.5);
+    ctx.scale(0.3, 0.3);
 
-    ctx.translate(200, 200);
-    ctx.drawImage(sprites.torso.img, 0, 0);
+    ctx.drawImage(sprites.idle[Math.floor(animCycle / 3)].img, 0, 0);
 
-    ctx.save();
-    ctx.translate(-20, -160);
-    ctx.drawImage(sprites.head.img, 0, 0);
     ctx.restore();
 
-    ctx.save();
-    const rightFootOffset = Math.sin(walkingCycle / 25 * Math.PI) * 3;
-    ctx.translate(160, 180 + rightFootOffset);
-    ctx.drawImage(sprites.foot.img, 0, 0);
-    ctx.restore();
-
-    ctx.save();
-    const leftFootOffset = Math.sin((25 + walkingCycle / 25) * Math.PI) * 3;
-    ctx.translate(40, 180 + leftFootOffset);
-    ctx.drawImage(sprites.foot.img, 0, 0);
-    ctx.restore();
-
-    ctx.restore();
-
-    walkingCycle++;
-    if (walkingCycle > 100) {
-        walkingCycle = 0;
+    animCycle++;
+    if (animCycle >= 30) {
+        animCycle = 0;
     }
 }
 
 function renderScene() {
     ctx.clearRect(0, 0, 1000, 600);
 
-    renderCharacter();
+    // renderCharacter();
+    renderSprite();
 
     window.requestAnimationFrame(renderScene);
 }
 
-Promise.all(Object.keys(sprites).map(sprite => new Promise((resolve, reject) => {
+const allSpriteFiles = Object.values(sprites)
+    .reduce((s1, s2) => s1.concat(s2));
+
+Promise.all(allSpriteFiles.map(spriteItem => new Promise((resolve, reject) => {
     const img = new Image();
     img.onload = data => {
-        sprites[sprite].img = img;
+        spriteItem.img = img;
         resolve();
     }
-    img.src = sprites[sprite].path;
+    img.src = spriteItem.filePath;
 })))
 .then(() => {
     window.requestAnimationFrame(renderScene);
 });
 
+document.addEventListener('keydown', evt => {
+    if (evt.keyCode === 39) {
+        isWalking = true;
+    }
+});
 
-
-
-
-
-// ctx.save();
-
-// ctx.translate(200, 200);
-
-// // torso
-// ctx.beginPath();
-// ctx.moveTo(0, 0);
-// ctx.lineTo(0, -100);
-// ctx.closePath();
-// ctx.stroke();
-
-// ctx.save();
-
-// // leg
-// // ctx.translate(100, 100);
-// ctx.beginPath();
-// ctx.moveTo(0, 0);
-// ctx.lineTo(50, 50);
-// ctx.closePath();
-// ctx.stroke();
-
-// ctx.restore();
-
-// ctx.restore();
+document.addEventListener('keyup', evt => {
+    if (evt.keyCode === 39) {
+        isWalking = false;
+    }
+});
